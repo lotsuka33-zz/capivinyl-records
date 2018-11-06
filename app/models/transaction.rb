f4ee37a3165ec21a14df53d87ac390e1ecd2ab29
Lucas Otsuka <otsuka.lucas@gmail.com>
2018-11-06 17:03:05 -0200

    Created validations and routes

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,10 @@
 class Transaction < ApplicationRecord
   belongs_to :vinyl
   belongs_to :user
+  has_one :owner, through: :vinyl, class_name: 'User', source: :user
+  validates :user_id, presence: true
+  validates :vinyl_id, presence: true
 end
+
+# has_many :transactions
+# has_many :lendings, through: :vinyls, class_name: 'Transaction', source: :transactions
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,19 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :vinyls, only: [:new, :create, :edit, :update, :index] do
+      resources :transactions, only: [:new, :create, :show, :index] do
+        collection do
+        # :lent, :borrowed, :pending,                        # collection => no restaurant id in URL
+          get 'lent', to: "transactions#lent"
+          get 'borrowed', to: "transactions#borrowed"
+          get 'pending', to: "transactions#pending"
+        end
+      end
+    end
+  end
+  resources :vinyls, only: [:destroy, :show, :index] do
+    resources :transactions, only: [:destroy]
+  end
 end
