1f695286fdd08a4b2d6da5e8171776f539e77d51
camila mizumoto <camila.yuri.m@gmail.com>
2018-11-07 11:53:37 -0200

    Treta

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @vinyls = Vinyl.all
   end
 end
--- app/controllers/vinyls_controller.rb
+++ app/controllers/vinyls_controller.rb
@@ -1,2 +1,59 @@
 class VinylsController < ApplicationController
+  before_action :set_vinyl, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @vinyls = Vinyl.all
+  #   if policy_scope(Vinyl).nil?
+  #     @vinyls = []
+  #   else
+  #   @vinyls = policy_scope(Vinyl)
+  #   end
+  end
+# geral
+  def show
+
+  end
+
+  def new
+    @vinyl = Vinyl.new
+    # authorize @vinyl
+  end
+
+  def create
+    @vinyl = Vinyl.new(vinyl_params)
+    @vinyl.user = current_user
+    # authorize @vinyl
+
+    if @vinyl.save
+      redirect_to vinyls_path(@vinyl), notice: 'Vinyl successfully created!'
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    @vinyl.update(vinyl_params)
+    redirect_to vinyl_path(@vinyl)
+  end
+
+# geral
+  def destroy
+    @vinyl.destroy
+    redirect_to vinyls_path
+  end
+
+  private
+
+  def set_vinyl
+    @vinyl = Vinyl.find(params[:id])
+    # authorize @vinyl
+  end
+
+  def vinyl_params
+    params.require(:vinyl).permit(:album, :year, :artist, :description, :genre)
+  end
 end
