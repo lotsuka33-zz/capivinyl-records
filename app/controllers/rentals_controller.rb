fcdca5a82a595e5b3ed2b0591525d75b8c0d7187
camila mizumoto <camila.yuri.m@gmail.com>
2018-11-08 12:10:00 -0200

    Create rental button

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,15 +1,17 @@
 class RentalsController < ApplicationController
-  before_action :set_rental, only: [:show, :destroy]
+  before_action :set_vinyl, only: [:show, :new, :create, :destroy]
   # before_action :all_transactions, only: [:index, :pending, :borrowed, :lent]
   def new
     @rental = Rental.new
+
   end
 
   def create
     @rental = Rental.new(rental_params)
+    @rental.vinyl = @vinyl
     @rental.user = current_user
     if @rental.save
-      redirect_to user_vinyl_rental_path(@rental.id)
+      redirect_to rentals_path
     else
       render :new
     end
@@ -23,6 +25,29 @@ class RentalsController < ApplicationController
   def show
   end
 
+  def destroy
+    user_id = @rental.vinyl.user.id
+    vinyl_id = @rental.vinyl.id
+    @rental.destroy
+    # @dose.destroy
+    redirect_to rentals_path
+  end
+
+  private
+
+  def set_vinyl
+    @vinyl = Vinyl.find(params[:vinyl_id])
+  end
+
+  def rental_params
+    params.require(:rental).permit(:user_id, :vinyl_id)
+
+  end
+  # def all_transactions
+  #   @transactions = Transaction.all
+  # end
+end
+
   # def pending
   #   # @transactions = Transaction.where()
   # end
@@ -34,25 +59,3 @@ class RentalsController < ApplicationController
   # def lent
   #   @rentals = Rental.where(vinyl: Vinyl.where(params[:id]))
   # end
-
-  def destroy
-    user_id = @rental.vinyl.user.id
-    vinyl_id = @rental.vinyl.id
-    @rental.destroy
-    # @dose.destroy
-    redirect_to rentals_path
-  end
-
-  private
-
-  def set_rental
-    @rental = Rental.find(params[:id])
-  end
-
-  def rental_params
-    params.require(:rental).permit(:user_id, :vinyl_id)
-  end
-  # def all_transactions
-  #   @transactions = Transaction.all
-  # end
-end
