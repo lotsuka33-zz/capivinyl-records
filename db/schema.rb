e377d3139568fbf0347f1015e6e5898e23ee4ae8
tabatalisboa <tabatalisboa@protonmail.com>
2018-11-09 14:32:29 -0200

    uns concertos ai

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_09_130341) do
+ActiveRecord::Schema.define(version: 2018_11_09_143327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2018_11_09_130341) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
+    t.string "photo"
     t.index ["user_id"], name: "index_vinyls_on_user_id"
   end
 
